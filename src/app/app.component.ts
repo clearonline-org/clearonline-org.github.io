e3244c1560d3571a7c9fd572ace6b869248b414c
Marcellin Nshimiyimana <mars@fusemachines.com>
2016-12-31 22:58:32 -0500

    fix name in the main app ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,12 @@
+/**
+* @Author: mars
+* @Date:   2016-12-31T22:12:26-05:00
+* @Last modified by:   mars
+* @Last modified time: 2016-12-31T22:50:00-05:00
+*/
+
+
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +15,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Clear Online';
 }
